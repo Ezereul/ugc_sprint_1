3f9f7cff295b54a1d60e085533eff88ed48fe1dc
LuchnikKek <ilua.kabeshov01@mail.ru>
2024-04-04 07:09:49 +0300

    Немного чище, логгер и чуточка рефакторинга

--- etl/main.py
+++ etl/main.py
@@ -1,11 +1,20 @@
 import asyncio
+import logging
 
 import msgspec
 from aiokafka import AIOKafkaConsumer
+from aiokafka.structs import TopicPartition
 
 from src.config import settings
 from src.constants import Topics
-from src.schemas import TOPIC_TO_SCHEMA
+from src.schemas import TOPIC_TO_SCHEMA, BaseEvent
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format=settings.log_format, level=settings.log_level)
+
+
+async def load_stub(topic: str, msgs: list):
+    logger.critical("CLICKHOUSE \"%s\" got new messages. Msgs=%s." % (topic, len(msgs)))
 
 
 async def start_consumer(topic: str):
@@ -20,35 +29,32 @@ async def start_consumer(topic: str):
 
     try:
         await consumer.start()
+        logger.debug("Consumer for topic \"%s\" was started successfully." % topic)
 
-        print("Consumer for topic \"%s\" was started successfully." % topic)
         while True:
-            topic_msgs = []
-            topic_partitions = {}
+            topic_msgs: list[type[BaseEvent]] = []
+            start_topic_partition_offset: dict[TopicPartition, int] = {}
             await consumer.seek_to_committed()
             result = await consumer.getmany(timeout_ms=settings.consumer_timeout_ms)
 
             if not result:
-                print('Topic \"%s\" has no result.' % topic)
+                logger.info('Topic \"%s\" has no records.' % topic)
                 await asyncio.sleep(settings.consumer_timeout_ms / 1000)
                 continue
 
             for partition, partition_msgs in result.items():
-                topic_partitions[partition] = partition_msgs[0].offset
+                start_topic_partition_offset[partition] = partition_msgs[0].offset
                 topic_msgs.extend([msg.value for msg in partition_msgs])
 
-            if topic_msgs:
-                print(result)
-                print(topic_msgs)
-                print(topic_partitions)
+            logger.debug(start_topic_partition_offset)
 
             if len(topic_msgs) < settings.consumer_min_batch_size:
-                print('Topic \"%s\" is sleeping. Found %s records.' % (topic, len(topic_msgs)))
-                await consumer.commit(topic_partitions)
+                logger.info('Topic \"%s\" is sleeping. Found %s records.' % (topic, len(topic_msgs)))
+                await consumer.commit(start_topic_partition_offset)
                 await asyncio.sleep(settings.consumer_timeout_ms / 1000)
                 continue
             else:
-                print("CLICKHOUSE \"%s\" got new messages. Msgs=%s." % (topic, len(topic_msgs)))
+                await load_stub(topic, topic_msgs)
                 await consumer.commit()
 
     finally:
--- etl/src/config.py
+++ etl/src/config.py
@@ -10,6 +10,8 @@ class ETLSettings(BaseSettings):
     kafka_url: str
     consumer_timeout_ms: int
     consumer_min_batch_size: int
+    log_level: str
+    log_format: str
 
     model_config = SettingsConfigDict(env_file=ENV_PATH, extra='ignore')
 
